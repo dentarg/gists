038efa08a6279146ecf23e3d4f9cb41578773f65
Patrik Ragnarsson <patrik@starkast.net>
2015-12-12 16:09:19 +0100

    stuff

--- gists/ruby-resolv-3456/test.rb
+++ gists/ruby-resolv-3456/test.rb
@@ -2,5 +2,10 @@ require "resolv"
 
 puts RUBY_DESCRIPTION
 
+host = "http://Disaster.MY"
+
 resolver = Resolv::DNS.new(nameserver: ["8.8.8.8", "8.8.4.4"])
+
 resolver.getname("64.4.6.100")
+
+addresses = resolver.getaddresses(host).map(&:to_s)
