3642d453ff1b1c945753a10356cb8e7283653850
Patrik Ragnarsson <patrik@starkast.net>
2016-09-12 18:25:57 +0200

    test #parse too

--- gists/addressable-issue-239/code.rb
+++ gists/addressable-issue-239/code.rb
@@ -2,3 +2,4 @@ require "addressable/uri"
 
 URL = "http://AcinusFallumTrompetumNullunCreditumVisumEstAtCuadLongumEtCefallumEst.com"
 p Addressable::URI.heuristic_parse(URL).normalized_host
+p Addressable::URI.parse(URL).normalized_host
