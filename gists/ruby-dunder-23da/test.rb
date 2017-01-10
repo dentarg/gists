d06dd242ecc571e67da0c5b8991ccabddf91f4fb
Patrik Ragnarsson <patrik@starkast.net>
2017-01-10 12:56:55 +0100

    more testing

--- gists/ruby-dunder-23da/test.rb
+++ gists/ruby-dunder-23da/test.rb
@@ -1,6 +1,15 @@
 puts 'RUBY_VERSION:'
 puts RUBY_VERSION
 
+puts '__FILE__:'
+puts __FILE__
+
+puts 'File.basename(__FILE__):'
+puts File.basename(__FILE__)
+
+puts '__dir__:'
+puts __dir__
+
 puts 'File.expand_path("..", __dir__):'
 puts File.expand_path("..", __dir__)
 
@@ -10,5 +19,27 @@ puts __dir__
 puts 'File.join(__dir__, "lib"):'
 puts File.join(__dir__, "lib")
 
+puts 'File.join(__FILE__, "lib"):'
+puts File.join(__FILE__, "lib")
+
+puts 'File.join("..", __FILE__):'
+puts File.join("..", __FILE__)
+
 puts 'File.expand_path("..", __FILE__):'
 puts File.expand_path("..", __FILE__)
+
+puts
+
+puts 'File.join("..", "..", __FILE__):'
+puts File.join("..", "..", __FILE__)
+
+puts
+
+puts 'File.expand_path("..", File.join("..", __FILE__)):'
+puts File.expand_path("..", File.join("..", __FILE__))
+puts
+puts 'File.expand_path("..", File.join("..", "..", __FILE__)):'
+puts File.expand_path("..", File.join("..", "..", __FILE__))
+puts
+puts 'File.expand_path("..", File.join("..", "..", __FILE__)):'
+puts File.expand_path("..", File.join("..", "..", __FILE__))
