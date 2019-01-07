2334fa717a462d8708c35cff6a1cb91d9821b10a
Patrik Ragnarsson <patrik@starkast.net>
2019-01-07 18:57:12 +0100

    tried the workaround in another place

--- gists/ruby-bug-15499/config/puma.rb
+++ gists/ruby-bug-15499/config/puma.rb
@@ -9,6 +9,7 @@ port        ENV["PORT"]     || 3000
 environment ENV["RACK_ENV"] || "development"
 
 app do |env|
+  # Thread.new { sleep } # this did not work on 2.6.0
   body = 'Hello, World!'
   [200, { 'Content-Type' => 'text/plain', 'Content-Length' => body.length.to_s }, [body]]
 end
